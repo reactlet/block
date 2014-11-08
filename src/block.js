7e01b9d31050f26200d97148a7885ed2011f4e5a
leapon <uestudio@gmail.com>
2014-11-07 19:38:06 -0500

    block update with components folded in

--- src/block.js
+++ src/block.js
@@ -1,5 +1,4 @@
-
-
+// Block component
 var WopBlock = React.createClass({
     name: 'wopblock',
     mixins: [getCommonMixin],
@@ -51,3 +50,117 @@ var WopBlock = React.createClass({
         );
     }
 });
+
+// Logo component
+var WopLogo = React.createClass({
+    name: 'woplogo',
+    mixins: [getCommonMixin],
+    
+    // attribute definitions
+    getAttributes: function() {
+        var attributes = [
+            { name:'boxClass', type:'string', required:false, defaultValue:'', note:'container CSS class' },
+            { name:'text', type:'string', required:false, defaultValue:' ', note:'display text' },
+            { name:'link', type:'string', required:false, defaultValue:'', note:'web link' },
+            { name:'backgroundImage', type:'string', required:false, defaultValue:'', note:'background image' }
+        ];
+        return attributes;
+    },
+    
+    render: function() {
+        return (
+            <div className={ this.state.containerClassNames.join(' ') } >
+                <WopBlock data={ this.state } />
+            </div>
+        );
+    }
+});
+
+
+// Toolbar component
+var WopToolbar = React.createClass({
+    name: 'woptoolbar',
+    mixins: [getCommonMixin],
+    
+    // attribute definitions
+    getAttributes: function() {
+        var attributes = [
+            { name:'boxClass', type:'string', required:false, defaultValue:'', note:'container CSS class' },
+            { name:'buttons', type:'array', required:false, defaultValue:'', note:'toolbar buttons' }
+        ];
+        return attributes;
+    },
+    
+    render: function() {
+        var links = [];
+        this.state.buttons.map(function(button) {
+            links.push(
+                <WopBlock data={ button } />
+            );
+        })
+        return (
+            <div className={ this.state.containerClassNames.join(' ') } >
+                { links }
+            </div>
+        );
+    }
+});
+
+
+// Header component
+var WopHeader = React.createClass({
+    name: 'wopheader',
+    mixins: [getCommonMixin],
+    
+    // attribute definitions
+    getAttributes: function() {
+        var attributes = [
+            { name:'boxClass', type:'string', required:false, defaultValue:'', note:'container CSS class' },
+            { name:'logo', type:'object', required:false, defaultValue:'', note:'logo data' },
+            { name:'toolbar', type:'object', required:false, defaultValue:'', note:'toolbar data' }
+        ];
+        return attributes;
+    },
+    
+    render: function() {
+        return (
+            <div className={ this.state.containerClassNames.join(' ') } >
+                <div className="col-md-4">
+                    <WopLogo data={ this.state.logo } />
+                </div>
+                <div className="col-md-8">
+                    <WopToolbar data={ this.state.toolbar } />
+                </div>
+            </div>
+        );
+    }
+});
+
+// Footer component
+var WopFooter = React.createClass({
+    name: 'wopfooter',
+    mixins: [getCommonMixin],
+    
+    getAttributes: function() {
+        var attributes = [
+            { name:'boxClass', type:'string', required:false, defaultValue:'', note:'container CSS class' },
+            { name:'mainToolbar', type:'object', required:false, defaultValue:'', note:'main toolbar data' },
+            { name:'socialToolbar', type:'object', required:false, defaultValue:'', note:'socail toolbar data' }
+        ];
+        return attributes;
+    },
+    
+    render: function() {
+        return (
+            <div className={ this.state.containerClassNames.join(' ') } >
+                <div className="col-md-10">
+                    <WopToolbar data={ this.state.mainToolbar } />
+                </div>
+                <div className="col-md-2">
+                    <WopToolbar data={ this.state.socialToolbar } />
+                </div>
+            </div>
+        );
+    }
+});
+
